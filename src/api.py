13812275e73e5e82c87c407c80aa5596daacd21e
Bianca Csan <32331590+ebiacsan@users.noreply.github.com>
2020-04-13 06:18:09 -0300

    Update: api.py

--- src/api.py
+++ src/api.py
@@ -1,1 +1,20 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+app = Flask(__name__)
+api = Api(app)
 
+
+class THGapi(Resource):
+    def get(self):
+        cpf = request.args['CPF']
+        datanascimento = request.args['DataNascimento']
+        nomeMae = request.args['NomeMae']
+        return "cpf={}datanascimento={}\nNomeMae={}".format(cpf+"\n", datanascimento,
+                                                            nomeMae)
+
+
+api.add_resource(THGapi, '/thg')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
